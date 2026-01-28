808d27b422969005534413abae88738ecf245c32
Name <email>
2026-01-28 14:48:23 -0600

    adjusted

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -79,8 +79,8 @@ const singleProduct = async (req, res) => {
         const { productId } = req.body
         const product = await productModel.findById(productId)
         res.json({success:true,product})
-
-    } catch (error) {
+    } 
+    catch (error) {
         console.log(error)
         res.json({ success: false, message: error.message })
     }
